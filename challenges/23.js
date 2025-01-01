5810c3c7c071654d32143db06fb0734a8094849d
Volodymyr Davii <vladimir.davii@gmail.com>
2025-01-01 15:03:06 +0200

    23 solution

---
+++ challenges/23.js
@@ -0,0 +1,36 @@
+// The elves are working on a system to verify children's gift lists 👧👦. However, some lists are incomplete and numbers are missing!
+//
+// Your task is to write a function that, given an array of numbers, finds all the numbers that are missing between 1 and n (where n is the size of the array or the highest number in the array).
+//
+// Keep in mind that:
+//
+//     Numbers may appear more than once and others may be missing
+// The array always contains positive integers
+// Counting always starts from 1
+// findMissingNumbers([1, 2, 4, 6])
+// // [3, 5]
+//
+// findMissingNumbers([4, 8, 7, 2])
+// // [1, 3, 5, 6]
+//
+// findMissingNumbers([3, 2, 1, 1])
+// // []
+//
+// findDisappearedNumbers([5, 5, 5, 3, 3, 2, 1])
+// // [4]
+
+/**
+ * @param {number[]} nums - List of integers.
+ * @returns {number[]} - List of missing numbers.
+ */
+function findMissingNumbers(nums) {
+    let max = Math.max(...nums);
+    let min = 1;
+    let missing = []
+    for (let i = min; i < max; i++) {
+        if (!nums.includes(i)) {
+            missing.push(i)
+        }
+    }
+    return missing
+}
